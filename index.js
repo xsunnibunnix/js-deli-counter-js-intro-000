4f43f4f74c93aac228946d1147ce6c81d5737abb
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-11 17:54:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ function nowServing(line) {
   if (line.length === 0) {
     return  "There is nobody waiting to be served!"
   } else {
-    function person () {
-      return `Currently serving ${line[0]}.`;}
+    do {
+      return `Currently serving ${line[0]}.`;
+    } while (line.length>0);
     line.shift(0);
   }
 }
