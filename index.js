71c2808ff4c963f0d09ca0211b53235ec0d8f10e
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-11 19:17:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line) {
     return "The line is currently empty."
   } else {
     for (let i = 0; i < line.length; i++) {
-      return `The line is currently: ` + i + `.` + line[i] + `,`;
+      return `The line is currently: ` + i+1 + `. ` + line[i] + `,`;
     }
   }
 }
