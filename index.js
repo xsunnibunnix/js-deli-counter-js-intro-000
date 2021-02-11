766e0b165c5b719921eaef5522330f38a3a07118
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-11 16:52:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(line, name) {
   line.push(name)
   return `Welcome, ${name}. You are number ` + (line.length) + ` in line.`
 }
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return  "There is nobody waiting to be served!"
+  } else {
+    return line[0]
+  }
+  line.shift()
+}
